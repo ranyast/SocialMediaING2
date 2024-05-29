5fd541843ec64bf0100bb38542f572e6dbc0a372
shaïma <114345002+shm0m@users.noreply.github.com>
2024-05-29 20:33:37 +0200

    Update login.php

--- backend/connexion/login.php
+++ backend/connexion/login.php
@@ -7,40 +7,46 @@
     <link rel="icon" href="logo/logo_ece.ico" type="image/x-icon" />
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-
 </head>
 <body>
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $mot_de_passe = $_POST['mot_de_passe'];
 
-$email = $_POST['email'];
-$mot_de_passe = $_POST['mot_de_passe'];
+    $servername = "localhost";
+    $username = "root";
+    $password_db = "";
+    $dbname = "ece_in";
 
+   
+    $conn = new mysqli($servername, $username, $password_db, $dbname);
 
-$servername = "localhost";
-$username = "root";
-$password_db = "";
-$dbname = "social_media";
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-$conn = new mysqli($servername, $username, $password_db, $dbname);
+    $stmt = $conn->prepare("SELECT mot_de_passe FROM utilisateur WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($hashed_password);
 
+    if ($stmt->num_rows > 0) {
+        $stmt->fetch();
+        if (password_verify($mot_de_passe, $hashed_password)) {
+            echo "Connexion réussie";
+            exit();
+        } else {
+            echo "Adresse e-mail ou mot de passe incorrect.";
+        }
+    } else {
+        echo "Adresse e-mail ou mot de passe incorrect.";
+    }
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $stmt->close();
+    $conn->close();
 }
-
-
-$sql = "SELECT * FROM utilisateur WHERE email='$email' AND mot_de_passe='$mot_de_passe'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    header("Location: /ProjetPiscine/frontend/accueil.html");
-    exit();
-} else {
-
-    echo "Adresse e-mail ou mot de passe incorrect.";
-}
-
-$conn->close();
 ?>
 </body>
 </html>
