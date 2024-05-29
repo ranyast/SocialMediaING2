978cc16c3021a934819d8fa37ccc9772697e7a2c
shaïma <114345002+shm0m@users.noreply.github.com>
2024-05-29 21:50:54 +0200

    Update register.php

--- backend/connexion/register.php
+++ backend/connexion/register.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>ECE In</title>
-    <meta charset="utf-8"/>
-    <link href="ECEIn.css" rel="stylesheet" type="text/css" />
-    <link rel="icon" href="logo/logo_ece.ico" type="image/x-icon" />
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-</head>
-<body>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $prenom = $_POST['prenom'];
@@ -16,8 +5,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $date_naissance = $_POST['date_naissance'];
     $email = $_POST['email'];
     $mot_de_passe = $_POST['mot_de_passe'];
+    $statut = $_POST['role']; // Récupérer le statut
 
-
+    // Hash the password
     $hashed_password = password_hash($mot_de_passe, PASSWORD_DEFAULT);
 
     $servername = "localhost";
@@ -25,18 +15,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password_db = "";
     $dbname = "ece_in";
 
+    // Create connection
     $conn = new mysqli($servername, $username, $password_db, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $stmt = $conn->prepare("INSERT INTO utilisateur (prenom, nom, date_naissance, email, mot_de_passe) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss", $prenom, $nom, $date_naissance, $email, $hashed_password);
+    // Prepare and bind the statement
+    $stmt = $conn->prepare("INSERT INTO utilisateur (prenom, nom, date_naissance, email, mot_de_passe, statut) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssi", $prenom, $nom, $date_naissance, $email, $hashed_password, $statut);
 
     if ($stmt->execute()) {
         echo "Inscription réussie";
-        header("Location: /ProjetPiscine/frontend/accueil.html");
+
         exit();
     } else {
         echo "Erreur lors de l'inscription : " . $stmt->error;
@@ -46,5 +38,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->close();
 }
 ?>
-</body>
-</html>
