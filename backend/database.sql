40e300ab13e3b63d0038ad94d024297dcb0b6d1a
shm0m <shaima.derouich@edu.ece.fr>
2024-05-30 12:47:18 +0200

    maj bdd

--- backend/database.sql
+++ backend/database.sql
@@ -12,7 +12,19 @@ CREATE TABLE utilisateur (
     etudes INT,
     sexe INT,
     competences TEXT,
+    formation TEXT,
     CONSTRAINT CHK_statut CHECK (statut IN (0, 1, 2)),
     CONSTRAINT CHK_etudes CHECK (etudes IN (0, 1, 2, 3, 4, 5, 6)),
     CONSTRAINT CHK_sexe CHECK (sexe IN (0, 1, 2))
+);
+
+
+CREATE TABLE amitie ( 
+    id_amitie INT AUTO_INCREMENT PRIMARY KEY, 
+    id_user1 INT NOT NULL, 
+    id_user2 INT NOT NULL, 
+    est_ami BOOLEAN NOT NULL, 
+    CONSTRAINT FK_user1 FOREIGN KEY (id_user1) REFERENCES utilisateur(id_user), 
+    CONSTRAINT FK_user2 FOREIGN KEY (id_user2) REFERENCES utilisateur(id_user) 
+
 );
