045b8c1a57e91e49e4047a941a2fbb2eb9dbfe1b
Ranya STITOU <ranyast@yahoo.fr>
2024-05-30 19:18:19 +0200

    modif de navigation avec agrandissement des icones + option de déconnexion

--- frontend/monreseau.php
+++ frontend/monreseau.php
@@ -22,15 +22,21 @@
                 <div class="col-sm-1" id="logo">
                     <h1><img src="logo/logo_ece.png" height="82" width="158" alt="Logo"></h1>
                 </div>
-                <div class="col-sm-11" id="logos">
+                <div class="col-sm-2" id="recherche" style="text-align: right">
+                    <p>Recherche</p>
+                </div>
+                <div class="col-sm-7" id="logos">
                     <nav>
-                        <a href="accueil.php"><img src="logo/accueil.jpg" height="56" width="100" alt="Accueil"></a>
-                        <a href="monreseau.php"><img src="logo/reseau2.jpg" height="56" width="100" alt="Réseau"></a>
-                        <a href="vous.php"><img src="logo/vous.jpg" height="56" width="100" alt="Vous"></a>
-                        <a href="notifications.php"><img src="logo/notification.jpg" height="56" width="100" alt="Notifications"></a>
-                        <a href="messagerie.php"><img src="logo/messagerie.jpg" height="56" width="100" alt="Messagerie"></a>
-                        <a href="emploi.php"><img src="logo/emploi.jpg" height="56" width="100" alt="Emploi"></a>
+                        <a href="accueil.php"><img src="logo/accueil.jpg" height="75" width="133" alt="Accueil"></a>
+                        <a href="monreseau.php"><img src="logo/reseau2.jpg" height="75" width="133" alt="Réseau"></a>
+                        <a href="vous.php"><img src="logo/vous.jpg" height="75" width="133" alt="Vous"></a>
+                        <a href="notifications.php"><img src="logo/notification.jpg" height="75" width="133" alt="Notifications"></a>
+                        <a href="messagerie.php"><img src="logo/messagerie.jpg" height="75" width="133" alt="Messagerie"></a>
+                        <a href="emploi.php"><img src="logo/emploi.jpg" height="75" width="133" alt="Emploi"></a>
                     </nav>
+                </div>
+                <div class="col-sm-1" id="deconnexion">
+                    <a href="../backend/connexion/connexion.html"><img src="logo/deconnexion.jpg" height="75" width="133" alt="Deconnexion"></a>
                 </div>
             </div>
         </div>
