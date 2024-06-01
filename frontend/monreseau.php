7e2174569ae2e0c81b4067b5e6039f41c1fb490e
ambrnsnssi <ambrine.senoussi@edu.ece.fr>
2024-06-01 18:56:49 +0200

    reseau + notif + css

--- frontend/monreseau.php
+++ frontend/monreseau.php
@@ -161,39 +161,43 @@ function respondToFriendRequest($request_id, $response, $conn) {
     <link href="ECEIn.css" rel="stylesheet" type="text/css" />
     <link rel="icon" href="logo/logo_ece.ico" type="image/x-icon" />
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        #nav{}
-        #footer{}
-        #wrapper{}
-        #section{}
-    </style>
 </head>
 <body>
 <div id="wrapper">
     <div id="nav">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-sm-1" id="logo">
+                <div class="col-sm-2" id="logo">
                     <h1><img src="logo/logo_ece.png" height="80" width="146" alt="Logo"></h1>
                 </div>
-                <div class="col-sm-2" id="recherche" style="text-align: right">
-                    <p>Recherche</p>
-                </div>
-                <div class="col-sm-9" id="logos">
+                <div class="col-sm-8" id="logos">
                     <nav>
                         <a href="accueil.php"><img src="logo/accueil.jpg" height="70" width="125" alt="Accueil"></a>
-                        <a href="monreseau.php"><img src="logo/reseau2.jpg" height="70" width="125" alt="Réseau"></a>
+                        <a href="monreseau.php"><img src="logo/reseau.jpg" height="70" width="125" alt="Réseau"></a>
                         <a href="vous.php"><img src="logo/vous.jpg" height="70" width="125" alt="Vous"></a>
-                        <a href="notifications.php"><img src="logo/notification.jpg" height="70" width="125" alt="Notifications"></a>
+                        <a href="notifications.php"><img src="logo/notification2.jpg" height="70" width="125" alt="Notifications"></a>
                         <a href="messagerie.php"><img src="logo/messagerie.jpg" height="70" width="125" alt="Messagerie"></a>
                         <a href="emploi.php"><img src="logo/emploi.jpg" height="70" width="125" alt="Emploi"></a>
-                        <a href="../backend/connexion/connexion.html"><img src="logo/deconnexion.jpg" height="70" width="125" alt="Deconnexion"></a>
                     </nav>
+                </div>
+                <div class="col-sm-2" id="logo">
+                    <a href="../backend/connexion/connexion.html"><img src="logo/deconnexion.jpg" height="70" width="125" alt="Deconnexion"></a>
                 </div>
             </div>
         </div>
     </div>
-    <div id="section">
+
+    <div id="leftcolumn">
+        <img src="<?php echo $photo_profil; ?>" class="img-circle" alt="Photo de profil" width="150">
+        <h3><?php echo $prenom . " " . $nom; ?></h3>
+        <p><?php echo $statut; ?></p>
+        <p>Email: <?php echo $email; ?></p>
+        <p>Etudes: <?php echo $etudes; ?></p>
+        <p>Sexe: <?php echo $sexe; ?></p>
+    </div>
+
+
+    <div id="section2">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-5" id="partieGauche">
@@ -251,30 +255,59 @@ function respondToFriendRequest($request_id, $response, $conn) {
                     <?php endif; ?>
                     <?php $stmt->close(); ?>
                 </div>
-
-                <div style="margin: 10px; padding: 10px" class="col-sm-12" id="partieDroite">
-                    <h3>Mes amis</h3>
-                    <?php if ($mutuals): ?>
-                        <ul>
-                            <?php foreach ($mutuals as $friend): ?>
-                                <li>
-                                    <a href="profil.php?id_user=<?= htmlspecialchars($friend['id_user']) ?>">
-                                        <?= htmlspecialchars($friend['prenom']) . ' ' . htmlspecialchars($friend['nom']) ?>
-                                    </a> 
-                                    (<?= htmlspecialchars($friend['email']) ?>)
-                                    <form action="monreseau.php" method="post" style="display:inline;">
-                                        <input type="hidden" name="remove_friend" value="<?= htmlspecialchars($friend['id_user']) ?>">
-                                        <button type="submit" name="removeFriend">Retirer de mes amis</button>
-                                    </form>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                    <?php else: ?>
-                        <p>Vous n'avez pas encore d'amis.</p>
-                    <?php endif; ?>
-                </div>
             </div>
         </div>
+    </div>
+
+    <div id="section2">
+        <h3>Mes amis</h3>
+        <?php if ($mutuals): ?>
+            <ul>
+                <?php foreach ($mutuals as $friend): ?>
+                    <li>
+                        <a href="profil.php?id_user=<?= htmlspecialchars($friend['id_user']) ?>">
+                            <?= htmlspecialchars($friend['prenom']) . ' ' . htmlspecialchars($friend['nom']) ?>
+                        </a>
+                        (<?= htmlspecialchars($friend['email']) ?>)
+                        <form action="monreseau.php" method="post" style="display:inline;">
+                            <input type="hidden" name="remove_friend" value="<?= htmlspecialchars($friend['id_user']) ?>">
+                            <button type="submit" name="removeFriend">Retirer de mes amis</button>
+                        </form>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php else: ?>
+            <p>Vous n'avez pas encore d'amis.</p>
+        <?php endif; ?>
+    </div>
+
+    <div id="footer">
+        <footer>
+            <h3>Nous Contacter: </h3>
+            <table>
+                <td style="padding-right:350px;padding-left:210px;">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.372438613096!2d2.285962676518711!
+                    3d48.85110800121897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b486bb253%3A0x61e9cc6979f93f
+                    ae!2s10%20Rue%20Sextius%20Michel%2C%2075015%20Paris!5e0!3m2!1sfr!2sfr!4v1716991235930!5m2!1sfr!2sfr"
+                            width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
+                </td>
+
+                <td style="font-size: 18px; text-align: center; padding :20px;">
+                    <p>Par Mail: <a href="mailto : ECEIN@ece.fr"> ECEIN@ece.fr</a></p>
+                    <p>Par Téléphone: <a href="tel:0144390600">01 44 39 06 00</a></p>
+                    <p>Notre Adresse: <a href="https://www.google.com/maps/place/10+Rue+Sextius+Michel,+75015+Paris/
+            @48.851108,2.2859627,17z/data=!3m1!4b1!4m6!3m5!1s0x47e6701b486bb253:0x61e9cc6979f93fae!8m2!3d48.
+            8511045!4d2.2885376!16s%2Fg%2F11bw3xcdpj?entry=ttu">10 Rue Sextius Michel, 75015 Paris</a></p>
+                </td>
+            </table>
+
+
+            <p>ECE In Corporation &copy; 2024</p>
+
+
+        </footer>
     </div>
 </div>
 </body>
