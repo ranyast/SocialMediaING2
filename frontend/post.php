baade38eebf5fdf0eeec33e69a16b96648b665fd
Anna Putod <anna.putod@edu.ece.fr>
2024-06-02 15:06:23 +0200

    commit de ce matin quand ca marchait

--- frontend/post.php
+++ frontend/post.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 
-// Vérifie si l'utilisateur est connecté et si les données POST sont définies
+//verifie si l'utilisateur est connecté
 if (isset($_SESSION['id_user']) && isset($_POST['message']) && isset($_POST['recipient_id'])) {
-    // Récupère les données POST
+    //recupere les parametres
     $message = $_POST['message'];
     $recipient_id = $_POST['recipient_id'];
     $sender_id = $_SESSION['id_user'];
 
-    // Connexion à la base de données
+    //connexion à la bdd
     $servername = "localhost";
     $username = "root";
     $password_db = "";
@@ -16,15 +16,15 @@ if (isset($_SESSION['id_user']) && isset($_POST['message']) && isset($_POST['rec
 
     $conn = new mysqli($servername, $username, $password_db, $dbname);
 
-    // Vérifie la connexion à la base de données
+    //verifie la connexion a la bdd
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Requête SQL pour insérer un message
-    $stmt = $conn->prepare("INSERT INTO messages (id_groupe, id_user, message) VALUES (?, ?, ?)");
-    // Liaison des paramètres
-    $stmt->bind_param("iis", $recipient_id, $sender_id, $message);
+    //requete sql pour insérer un message
+    $stmt = $conn->prepare("INSERT INTO messages (sender, recipient, message) VALUES (?, ?, ?)");
+    //liaison des paramètres
+    $stmt->bind_param("iis", $sender_id, $recipient_id, $message);
     $stmt->execute();
     $stmt->close();
     $conn->close();
