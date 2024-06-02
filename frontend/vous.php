a0dbbfbdd2f58a27fe26ff57bfed2ffecba79320
ambrnsnssi <ambrine.senoussi@edu.ece.fr>
2024-06-02 23:02:11 +0200

    commentaire2

--- frontend/vous.php
+++ frontend/vous.php
@@ -179,7 +179,7 @@ $conn->close();
     </div>
 
     <div id="section">
-        //affichage photo en rond+ nom/prenom/staut a partir de la bdd
+        <!--affichage photo en rond+ nom/prenom/staut a partir de la bdd-->
         <div class="media">
             <div class="media-left">
                 <?php if (!empty($photo_profil)): ?>
@@ -196,13 +196,13 @@ $conn->close();
         </div>
         <br>
         <br>
-        //utilisation de boostrap pour les cases
+        <!--utilisation de boostrap pour les cases-->
         <div class="container-fluid" >
             <div class="row">
                 <font size="3">
                 <form method="post" action="">
                     <div id="infoprofil">
-                        //recup des info de la partie modif
+                        <!--recup des info de la partie modif-->
                         <div class="col-sm-8 case3" id="description">
                             <h3>Description</h3>
                             <p><?php echo htmlspecialchars($description ?? ''); ?></p>
@@ -232,7 +232,7 @@ $conn->close();
             </div>
             <br>
 
-            //partie modif a remplir par utilisateur
+            <!--partie modif a remplir par utilisateur-->
             <div class="col-sm-12 case2" id="modifprofil">
                 <h1 style="text-align: center;">Modification</h1>
                 <br>
@@ -251,7 +251,7 @@ $conn->close();
                         <textarea name="experience"></textarea>
                     </div>
                     <div class="col-sm-4 case" id="etudes_modif">
-                        //liste selection niv
+                        <!--liste selection niv-->
                         <h3>Études</h3>
                         <select name="etudes" required>
                             <option value="0" <?php if ($etudes == '0') echo 'selected'; ?>>Bac</option>
@@ -287,7 +287,7 @@ $conn->close();
                     <button type="submit" style="background-color: #028E98; border: none" class="btn btn-primary" value="validation"><font size="3"> Enregistrer</font></button>
                 </form>
             </div>
-            //bouton generer le cv
+            <!--bouton generer le cv-->
             <div class="col-sm-12" style="text-align: right">
                 <form method="post" action="cv/generercv.php">
                     <button type="submit" style="background-color: #028E98; border: none" class="btn btn-primary"><font size="3"> Générer mon CV</font></button>
